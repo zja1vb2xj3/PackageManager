4012cd2402e89eeb9b8ffc420451f9971b2a3e7e
pdg <zja1vb2xj3@naver.com>
2017-07-26 18:05:23 +0900

    커밋테스트

--- app/src/main/java/com/example/administrator/packagemanager/MainActivity.java
+++ app/src/main/java/com/example/administrator/packagemanager/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //커밋테스트
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
