d94fb170f2e29d8fde49d66b3a0607acde0bb6ec
pdg <zja1vb2xj3@naver.com>
2017-07-29 12:46:13 +0900

    뷰는 메모리에 계속 남겨두면 안됨..

--- app/src/main/java/com/example/administrator/packagemanager/PackageObserver.java
+++ app/src/main/java/com/example/administrator/packagemanager/PackageObserver.java
@@ -9,26 +9,26 @@ import android.widget.TextView;
 public class PackageObserver implements PackageObserverModel.IValueObserver {
 
     private PackageObserverModel packageObserverModel;
-    private TextView textView;
+    private TextView packageCount_TextView;
 
     public PackageObserver(TextView textView) {
         packageObserverModel = PackageObserverModel.getInstance();
-        this.textView = textView;
+        this.packageCount_TextView = textView;
     }
 
-    public TextView getTextView() {
-        return textView;
+    public TextView getPackageCount_TextView() {
+        return packageCount_TextView;
     }
 
-    public void setTextView(TextView textView) {
-        this.textView = textView;
+    public void setPackageCount_TextView(TextView packageCount_TextView) {
+        this.packageCount_TextView = packageCount_TextView;
     }
 
     @Override
     public void onChanged(int beforeValue, int afterValue) {
         int initialValue = packageObserverModel.getInitialValue();
 
-        textView.setText(
+        packageCount_TextView.setText(
                 "초기 패키지 수 : " + String.valueOf(initialValue) + "\n" +
                 "이전 패키지 수 : " + String.valueOf(beforeValue) + "\n" +
                 "변경 패키지 수 : " + String.valueOf(afterValue)
