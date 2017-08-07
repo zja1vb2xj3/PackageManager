5584884e6795ad5e1580a9ce9bcc5084cd467f92
PDG <zja1vb2xj3@naver.com>
2017-08-08 07:27:11 +0900

    dialog생성소스코드 MainActivity에서 변경

--- app/src/main/java/com/example/administrator/packagemanager/MainActivity.java
+++ app/src/main/java/com/example/administrator/packagemanager/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.administrator.packagemanager;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.List;
@@ -51,10 +55,108 @@ public class MainActivity extends Activity {
         //LayoutManager : RecyclerView내에 item view들의 크기 측정 및 위치 지정
         //언제 view들을 재사용해야하는지 결정
         adapter = new RecyclerViewAdapter(this, packageNames, packageCount_TextView);
+
+        adapter.setCreateDialogListener(new RecyclerViewAdapter.createDialogListener() {
+            @Override
+            public void onPackageInfoDialog(String packageName) {
+                createPackageInfoDialog(packageName);
+            }
+
+            @Override
+            public void onAskedToRemoveDialog(String packageName) {
+                createAskedToRemoveDialog(packageName);
+            }
+
+
+        });
         //Adapter : RecyclerView내에 보여지는 view들의 data set을 binding해주는 역할
         recyclerView.setAdapter(adapter);
     }
 
+
+    //longClick 시 패키지 삭제 물음
+    private void createAskedToRemoveDialog(String packageName) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        final LayoutInflater factory = LayoutInflater.from(this);
+        final View view = factory.inflate(R.layout.dialog_askedtoremove, null);
+
+        final ImageView deletePackageIcon = (ImageView) view.findViewById(R.id.deletePackageIcon);
+        final TextView deletePackageName = (TextView) view.findViewById(R.id.deletePackageName);
+
+        setDialog(deletePackageIcon,deletePackageName,null, packageName);
+
+        builder.setTitle("선택한 Package 를 삭제 하시겠습니까?").setView(view).setCancelable(false)
+                .setPositiveButton("삭제", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        boolean removeSign = adapter.removeDataList(adapter.getClickPosition());
+
+                        if (removeSign != false) {
+                            Toast.makeText(getApplicationContext(), "선택한 패키지가 삭제되었습니다. \n패키지 개수 = " + adapter.getItemCount(), Toast.LENGTH_LONG).show();
+                        } else
+                            Toast.makeText(getApplicationContext(), "패키지 삭제 중 오류가 발생하였습니다.", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+    private void setDialog(ImageView packageIcon, TextView packageName, TextView packageInfo, String pakageName) {
+        if (packageIcon != null)
+            packageIcon.setImageDrawable(myPackage.getPackageIcon(pakageName));
+
+        if (packageName != null) {
+            packageName.setText(String.format("%s", pakageName));
+        }
+
+        if (packageInfo != null) {
+            packageInfo.setText(
+                    "App 이름 : " + myPackage.getPackageAppName(pakageName) + "\n\n" +
+                            "App 크기 : " + myPackage.getPackageInstalledFileSize(pakageName) + "\n\n" +
+                            "App 버전 : " + myPackage.getPackageVersion(pakageName) + "\n\n" +
+                            "설치 날짜 : " + myPackage.getPackageFirstInstallTime(pakageName) + "\n\n" +
+                            "수정 날짜 : " + myPackage.getPackageLastUpdateTime(pakageName) + "\n\n"
+            );
+        }
+    }
+
+    //onClick 시 패키지 정보 보여주기
+    private void createPackageInfoDialog(String pakageName) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater factory = LayoutInflater.from(this);
+        final View view = factory.inflate(R.layout.dialog_packageinfo, null);
+
+        final ImageView packageIcon = (ImageView) view.findViewById(R.id.selectedPackageIcon);
+        final TextView packageName = (TextView) view.findViewById(R.id.selectedPackageName);
+        final TextView packageInfo = (TextView) view.findViewById(R.id.selectedPackageInfo);
+
+        setDialog(packageIcon, packageName, packageInfo, pakageName);
+
+        builder.setTitle("선택한 Package 정보")
+                .setView(view)
+                .setCancelable(false)
+                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
 
 
     private void resetButtonClick(View view) {
--- app/src/main/java/com/example/administrator/packagemanager/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/administrator/packagemanager/RecyclerViewAdapter.java
@@ -1,16 +1,15 @@
 package com.example.administrator.packagemanager;
 
 import android.content.Context;
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import java.util.List;
+import java.util.Vector;
 
 /**
  * Created by ADMIN on 2017-07-25.
@@ -21,6 +20,8 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
     private LayoutInflater layoutInflater;
     private MyPackage myPackage;
     private TextView packageCount_TextView;
+    private int clickPosition;
+    private ViewHolder viewHolder;
 
     public RecyclerViewAdapter(Context context, List<String> dataList, TextView packageCount_TextView) {
         this.layoutInflater = LayoutInflater.from(context);
@@ -39,7 +40,7 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = layoutInflater.inflate(R.layout.recyclerview_row, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder = new ViewHolder(view);
 
         return viewHolder;
     }
@@ -51,6 +52,18 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
         holder.packageName_TextView.setText(packageName);
         holder.packageIcon_ImageView.setImageDrawable(myPackage.getPackageIcon(packageName));
     }
+
+    public int getClickPosition() {
+        return clickPosition;
+    }
+
+    public boolean removeDataList(int position){
+        dataList.remove(position);
+        viewHolder.removeViewItem(position);
+
+        return true;
+    }
+
 
     //RecyclerView내에 각 item의 view 정보를 갖고있는 class
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
@@ -75,109 +88,27 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
 
             packageObserverModel = PackageObserverModel.getInstance();
             packageObserverModel.addObserver(new PackageObserver(packageCount_TextView));
+
         }
 
         @Override
         public void onClick(View v) {
-            createPackageInfoDialog();
+            createDialogListener.onPackageInfoDialog(dataList.get(getAdapterPosition()));
         }
 
-        //onClick 시 패키지 정보 보여주기
-        private void createPackageInfoDialog() {
-            final AlertDialog.Builder builder = new AlertDialog.Builder(context);
-
-            LayoutInflater factory = LayoutInflater.from(context);
-            final View view = factory.inflate(R.layout.dialog_packageinfo, null);
-
-            final ImageView packageIcon = (ImageView) view.findViewById(R.id.selectedPackageIcon);
-            final TextView packageName = (TextView) view.findViewById(R.id.selectedPackageName);
-            final TextView packageInfo = (TextView) view.findViewById(R.id.selectedPackageInfo);
-
-            setDialog(packageIcon, packageName, packageInfo);
-
-            builder.setTitle("선택한 Package 정보")
-                    .setView(view)
-                    .setCancelable(false)
-                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
-
-                        }
-                    });
-            AlertDialog dialog = builder.create();
-            dialog.show();
-
-        }
 
         @Override
         public boolean onLongClick(View v) {
-            createAskedToRemoveDialog(v);
-
+            createDialogListener.onAskedToRemoveDialog(dataList.get(getAdapterPosition()));
+            clickPosition = getAdapterPosition();
+            notifyDataSetChanged();
             return true;
         }
 
-
-        //longClick 시 패키지 삭제 물음
-        private void createAskedToRemoveDialog(View v) {
-            final AlertDialog.Builder builder = new AlertDialog.Builder(context);
-
-            final LayoutInflater factory = LayoutInflater.from(context);
-            final View view = factory.inflate(R.layout.dialog_askedtoremove, null);
-
-            final ImageView deletePackageIcon = (ImageView) view.findViewById(R.id.deletePackageIcon);
-            final TextView deletePackageName = (TextView) view.findViewById(R.id.deletePackageName);
-
-            setDialog(deletePackageIcon, deletePackageName, null);
-
-            builder.setTitle("선택한 Package 를 삭제 하시겠습니까?").setView(view).setCancelable(false)
-                    .setPositiveButton("삭제", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            boolean removeSign = removePackage(getAdapterPosition());
-
-                            if (removeSign != false) {
-                                Toast.makeText(v.getContext(), "선택한 패키지가 삭제되었습니다. \n패키지 개수 = " + dataList.size(), Toast.LENGTH_LONG).show();
-                            } else
-                                Toast.makeText(v.getContext(), "패키지 삭제 중 오류가 발생하였습니다.", Toast.LENGTH_LONG).show();
-                        }
-                    })
-                    .setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.cancel();
-                        }
-                    });
-
-
-            AlertDialog dialog = builder.create();
-            dialog.show();
-        }
-
-        private void setDialog(ImageView packageIcon, TextView packageName, TextView packageInfo) {
-            if (packageIcon != null)
-                packageIcon.setImageDrawable(myPackage.getPackageIcon(dataList.get(getAdapterPosition())));
-
-            if (packageName != null) {
-                packageName.setText(String.format("%s", dataList.get(getAdapterPosition())));
-            }
-
-            if (packageInfo != null) {
-                packageInfo.setText(
-                        "App 이름 : " + myPackage.getPackageAppName(dataList.get(getAdapterPosition())) + "\n\n" +
-                                "App 크기 : " + myPackage.getPackageInstalledFileSize(dataList.get(getAdapterPosition())) + "\n\n" +
-                                "App 버전 : " + myPackage.getPackageVersion(dataList.get(getAdapterPosition())) + "\n\n" +
-                                "설치 날짜 : " + myPackage.getPackageFirstInstallTime(dataList.get(getAdapterPosition())) + "\n\n" +
-                                "수정 날짜 : " + myPackage.getPackageLastUpdateTime(dataList.get(getAdapterPosition())) + "\n\n"
-                );
-            }
-        }
-
-        private boolean removePackage(int position) {
+         boolean removeViewItem(int position) {
             try {
                 packageObserverModel.setBeforeValue(dataList.size());
 
-                dataList.remove(position);
                 notifyItemRemoved(position); //등록된 옵저버에 이전에 위치햇던 항목이 데이트 세트에서 제거되었음을 알림.
 
                 packageObserverModel.setAfterValue(dataList.size());
@@ -190,6 +121,18 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewH
             }
             return false;
         }
+
+    }//end ViewHolder
+
+    private createDialogListener createDialogListener;
+
+    public void setCreateDialogListener(RecyclerViewAdapter.createDialogListener createDialogListener) {
+        this.createDialogListener = createDialogListener;
+    }
+
+    interface createDialogListener{
+        void onPackageInfoDialog(String packageName);
+        void onAskedToRemoveDialog(String pakageName);
     }
 
 }//end recyclerviewadapter
